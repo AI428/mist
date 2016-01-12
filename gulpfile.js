1ba426af51be6cd58c1167a2a3ee3c8e465afbed
Akihiro Ikeda <akihiro.ikeda.428@gmail.com>
2016-01-12 16:16:21 +0900

    Refactor.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,33 @@
 'use strict';
 
+// globals modules.
+
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var typescript = require('gulp-typescript');
 var uglify = require('gulp-uglify');
 
+// globals consts.
+
+var DIST_DIR = '';
+var DIST_MIN_EXT = '.min.js';
+var DIST_NAME = 'mist.js';
+var NAME = 'modules/mist.ts';
+
 gulp.task('default',
 
   function() {
 
-    gulp.src('modules/mist.ts')
+    gulp.src(NAME)
       .pipe(
         typescript({
-          out: 'mist.js',
+          out: DIST_NAME,
           target: 'ES5'
         })
       )
       .pipe(
         gulp.dest(
-          ''
+          DIST_DIR
         )
       )
       .pipe(
@@ -28,12 +37,12 @@ gulp.task('default',
       )
       .pipe(
         rename({
-          extname: '.min.js'
+          extname: DIST_MIN_EXT
         })
       )
       .pipe(
         gulp.dest(
-          ''
+          DIST_DIR
         )
       );
   });
