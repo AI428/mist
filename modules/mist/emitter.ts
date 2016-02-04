9d4c75035b766ff027d89f033a4d1fdab18b48e1
Akihiro Ikeda <akihiro.ikeda.428@gmail.com>
2016-02-04 17:49:54 +0900

    element.ts was added. Fork from emitter.ts

--- modules/mist/emitter.ts
+++ modules/mist/emitter.ts
@@ -1,3 +1,4 @@
+/// <reference path='element.ts'/>
 /// <reference path='statement.ts'/>
 
 namespace Mist {
@@ -102,63 +103,3 @@ namespace Mist {
     }
   }
 }
-
-/**
-* @class Element
-* @summary for vendor
-*/
-interface Element {
-
-  /**
-  * @param {} selector
-  * @return {}
-  */
-  closest: (selector: string) => Element;
-
-  /**
-  * @param {} selector
-  * @return {}
-  */
-  matches: (selector: string) => boolean;
-
-  /**
-  * @param {} selector
-  * @return {}
-  */
-  mozMatchesSelector: (selector: string) => boolean;
-}
-
-/**
-* @class Element
-* @method Element.matches
-*/
-(function(p: Element) {
-
-  p.matches = p.matches
-  || p.mozMatchesSelector
-  || p.msMatchesSelector
-  || p.webkitMatchesSelector;
-
-})(Element.prototype);
-
-/**
-* @class Element
-* @method Element.closest
-*/
-(function(p: Element) {
-
-  p.closest = p.closest || function(selector: string) {
-
-    var s = this;
-
-    // ref response.
-
-    while (s) {
-      if (s.matches(selector)) break;
-      s = s.parentElement;
-    }
-
-    return s;
-  };
-
-})(Element.prototype);
