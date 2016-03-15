48e3a05dd37f47c4574830c4e211bca67bf53ab9
Akihiro Ikeda <akihiro.ikeda.428@gmail.com>
2016-03-16 03:45:54 +0900

    wrapper modules are added.

---
+++ modules/mist/wrapper/voker.ts
@@ -0,0 +1,47 @@
+namespace Mist {
+
+  export namespace Wrapper {
+
+    /**
+    * @class Voker
+    * @namespace Wrapper
+    */
+    export class Voker {
+
+      /**
+      * @constructor
+      * @param {} component
+      */
+      constructor(private component: any) {
+
+        var s: any = this;
+
+        for (let name in component) {
+
+          if (component[name] instanceof Function) {
+
+            // lazy response.
+            function composer(...o: any[]) {
+              return s.compose$(
+                component[name].bind(
+                  component), o);
+            }
+
+            // {} response.
+            s[name] = composer;
+          }
+        }
+      }
+
+      /**
+      * @param {} composer
+      * @param {} o
+      * @summary for override
+      */
+      compose$(composer: any, o: any[]) {
+
+        return composer.apply(composer, o);
+      }
+    }
+  }
+}
