5656dea54d88c443f24d96372c9d99833073808b
Akihiro Ikeda <akihiro.ikeda.428@gmail.com>
2016-08-07 00:11:06 +0900

    Change path

--- doc/icon.js
+++ doc/icon.js
@@ -3,7 +3,7 @@
     var e = document.createElement('footer');
 
     e.setAttribute('style', 'bottom: 1em; left: 50%; transform: translate3d(-50%, 0, 0); position: absolute;');
-    e.innerHTML = "<a alt='MIST' href='//github.com/AI428/mist' title='MIST, Motion Design in Modular CSS'><img src='img/icon.png' style='height: 2em; width: 2em;'></a>";
+    e.innerHTML = "<a alt='MIST' href='//github.com/AI428/mist' title='MIST, Motion Design in Modular CSS'><img src='//rawgit.com/AI428/mist/master/doc/img/icon.png' style='height: 2em; width: 2em;'></a>";
 
     document.body.appendChild(e);
 })();
