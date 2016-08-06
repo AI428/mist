f67cd9ebe4736f9b21ed281b736d05375423fa1f
Akihiro Ikeda <akihiro.ikeda.428@gmail.com>
2016-08-07 01:06:46 +0900

    Refactor

--- doc/icon.js
+++ doc/icon.js
@@ -1,9 +1,22 @@
 (function() {
 
-    var e = document.createElement('footer');
+    var d = document.createElement('div');
 
-    e.setAttribute('style', 'bottom: 1em; left: 50%; transform: translate3d(-50%, 0, 0); position: absolute;');
-    e.innerHTML = "<a alt='MIST' href='//github.com/AI428/mist' title='MIST, Motion Design in Modular CSS'><img src='//rawgit.com/AI428/mist/master/doc/img/icon.png' style='height: 2em; width: 2em;'></a>";
+    d.setAttribute('style', 'left: 1em; position: absolute; top: 1em;');
 
-    document.body.appendChild(e);
+    var a = document.createElement('a');
+
+    a.setAttribute('alt', 'MIST');
+    a.setAttribute('href', '//github.com/AI428/mist');
+    a.setAttribute('title', 'MIST, Motion Design in Modular CSS');
+
+    var i = document.createElement('img');
+
+    i.setAttribute('src', '//rawgit.com/AI428/mist/master/doc/img/icon.png');
+    i.setAttribute('style', 'height: 2em; width: 2em;');
+
+    a.appendChild(i);
+    d.appendChild(a);
+
+    document.body.appendChild(d);
 })();
